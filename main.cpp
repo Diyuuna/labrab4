f8c62ab92da13696f90f7bc5e3e22bb7fe760e45
Diyuuna <d.ostanina18@icloud.com>
2025-12-14 22:00:33 +0400

    Добавлен каркас программы

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+vector<string> readFromFile(const string& filename)
+{
+    vector<string> lines;
+    return lines;
+}
+
+void printToScreen(const vector<string>& lines)
+{
+    cout<<endl;
+}
+
+void writeToFile(const vector<string>& lines, const string& filename)
+{
+    cout<<endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string inputFile = "input.txt";
+    string outputFile = "output.txt";
+
+    vector<string> lines = readFromFile(inputFile);
+    printToScreen(lines);
+    writeToFile(lines, outputFile);
+
+    wcout << L"Программа завершена." << endl;
     return 0;
 }
